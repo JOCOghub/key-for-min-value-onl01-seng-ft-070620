3a796cc4c2d5a6dabe4acea4cae0cb4a10cdacaa
Joseph cohen <jojocohen57@gmail.com>
2020-07-09 02:00:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,12 @@ def key_for_min_value(hash)
   if hash.empty?
     return nil
   end
-  return hash.first[1]
-  # ans = [1000,nil]
-  # hash.each do |k,v|
-  #   if v < ans[0]
-  #     ans[0] = v
-  #     ans[1] = k
-  #   end
-  # end
-  # return ans[1]
+  ans = [hash.first[1],nil]
+  hash.each do |k,v|
+    if v < ans[0]
+      ans[0] = v
+      ans[1] = k
+    end
+  end
+  return ans[1]
 end  
